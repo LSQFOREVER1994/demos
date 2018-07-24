49019bd1f621e379bc8e18916af2ac97cb9ee103
LSQFOREVER1994 <lsqltt@126.com>
2018-07-24 21:57:52 +0800

    complete end

--- demo5-Dynamic creation tag/showPic.js
+++ demo5-Dynamic creation tag/showPic.js
@@ -1,3 +1,39 @@
+function addLoadEvent(func){
+	var oldonload=window.onload;
+	if(typeof window.onload!='function'){
+		window.onload=func;
+	}else{
+		window.onload=function(){
+			oldonload();
+			func();
+		}
+	}
+}
+function insertAfter(newElement,targetElement){
+	var parent=targetElement.parentNode;
+	if(parent.lastChild==targetElement){
+		parent.appendChild(newElement);
+	}else{
+		parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
+function prepareplaceholder(){
+	if(!document.createElement)return false;
+	if(!document.createTextNode)return false;
+	if(!document.getElementById)return false;
+	if(!document.getElementById("imagegallery"))return false;
+	var placeholder=document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/cat.jpg");
+	placeholder.setAttribute("alt","my image gallery");
+	var description=document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext=document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	var gallery=document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
 function prepareGallery() {
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
@@ -25,3 +61,5 @@ function showPic(whichpic){
 	}
 	return true;
 }
+addLoadEvent(prepareplaceholder);
+addLoadEvent(prepareGallery);
