a6358418d24c6c0b3dd2e017328d84c47226d4ad
LSQFOREVER1994 <lsqltt@126.com>
2018-07-24 16:17:03 +0800

    createElement para.nodeName & para.nodeType

--- demo5-Dynamic creation tag/example.js
+++ demo5-Dynamic creation tag/example.js
@@ -1,11 +1,9 @@
-window.onload=function() {
-	var testdiv=document.getElementById("testdiv");
-	testdiv.innerHTML="<p>I inserted <em>this</em> content.</p>";
-	alert(testdiv.innerHTML);
-}
-function insertParagraph(text){
-	var str="<p>";
-	str+=text;
-	str+="</p>";
-	document.write(str);
+
+window.onload=function (){
+	var para=document.createElement("p");
+	var info="nodeName:";
+	info+= para.nodeName;
+	info+="nodeType:";
+	info+= para.nodeType;
+	alert(info);
 }
