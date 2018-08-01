0fed798078cbb54a477a031b1d341ca416c3f9b9
LSQFOREVER1994 <lsqltt@126.com>
2018-08-01 20:30:13 +0800

    change value

--- demo8-Achieve animation effect/positionMessage.js
+++ demo8-Achieve animation effect/positionMessage.js
@@ -5,6 +5,6 @@ function positionMessage() {
 	elem.style.position="absolute";
 	elem.style.left="50px";
 	elem.style.top="100px";
-	moveElement("message",200,100,10);
+	moveElement("message",125,25,20);
 }
 addLoadEvent(positionMessage);
